c47d9b83ddf2c84d57bc252be6a2baaf7d90a34d
Ryan Wibawa <ryan.wibawa@gmail.com>
2025-01-15 20:50:08 -0800

    Add MergeTwoLists.

---
+++ src/MergeTwoLists.java
@@ -0,0 +1,39 @@
+public class MergeTwoLists {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        MergeTwoLists app = new MergeTwoLists();
+        ListNode result = app.merge(l1, l2);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l2.next, l1);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
